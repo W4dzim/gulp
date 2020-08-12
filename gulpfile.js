cd05c7c9206e9be8e8f8a29b093656d271a1b34c
Vadim <selivonchik91@gmail.com>
2020-08-12 15:52:26 +0300

    gulp build and del added

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,12 @@ let gulp = require('gulp'),
     browserSync = require('browser-sync'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    del = require('del');
+
+    gulp.task('clean', function() {
+        del.sync('dist')
+    });
 
 gulp.task('scss', function(){
     return gulp.src('app/scss/**/*.scss') 
@@ -16,8 +21,7 @@ gulp.task('scss', function(){
 gulp.task('css', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/slick-carousel/slick/slick.css'
     ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -36,8 +40,7 @@ gulp.task('script', function() {
 
 gulp.task('js', function(){
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/slick-carousel/slick/slick.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -51,6 +54,24 @@ gulp.task('browser-sync', function() {
             baseDir: "app/"
         }
     });
+});
+
+gulp.task('build', async function() {
+    let buildHtml = gulp.src('app/**/*.html')
+        .pipe(gulp.dest('dist'));
+
+    let buildCss = gulp.src('app/css/**/*.css')
+        .pipe(gulp.dest('dist/css'));
+
+    let buildJs = gulp.src('app/css/**/*.js')
+        .pipe(gulp.dest('dist/js'));
+
+    let buildFonts = gulp.src('app/fonts/**/*.*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    let buildImg = gulp.src('app/img/**/*.*')
+        .pipe(gulp.dest('dist/img'));
+        
 });
 
 gulp.task('watch', function(){
